e75ac56d143faef78115fe188b0c8597398aac7b
ahmedmustahid <einsteinium25@gmail.com>
2025-05-22 21:05:44 +0900

    deployed mcp tools in azure function

--- client.py
+++ client.py
@@ -1,12 +1,13 @@
 import asyncio
+import json
 import os
 
 from dotenv import load_dotenv
+from langchain_core.load import dumps
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain_openai import AzureChatOpenAI
 from langgraph.prebuilt import create_react_agent
 from mcp import ClientSession
-from mcp.client.streamable_http import streamablehttp_client
 from mcp.client.sse import sse_client
 
 load_dotenv()
@@ -18,10 +19,8 @@ async def main():
         write,
     ):
         async with ClientSession(read, write) as session:
-            # セッションを初期化
             await session.initialize()
 
-            # MCPからtools情報を取得
             tools = await load_mcp_tools(session)
             deployment_name = os.getenv("AZURE_OPENAI_DEPLOYMENT")
             api_version = os.getenv("AZURE_OPENAI_API_VERSION")
@@ -31,17 +30,24 @@ async def main():
                 timeout=None,
                 max_retries=2,
             )
-
             agent = create_react_agent(llm, tools)
 
             response = await agent.ainvoke(
                 {
                     "messages": [
-                        {"role": "user", "content": "what's (3 + 5) x 12?"}
+                        {
+                            "role": "user",
+                            "content": "get the foreign keys from the postgres database",
+                        }
                     ]
                 }
             )
-            print(response)
+            result = dumps(response, pretty=True)
+            data = json.loads(result)
+
+            # Write the data to a JSON file
+            with open("result.json", "w") as json_file:
+                json.dump(data, json_file, indent=4)
 
 
 if __name__ == "__main__":
